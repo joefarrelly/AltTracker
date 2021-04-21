4f47acb2f00ac8ac6107cd14b757c967e9a1359c
Joe Farrelly <joefarrelly96@gmail.com>
2021-04-21 22:36:00 +0100

    added DB HOST to env

--- alttracker/settings.py
+++ alttracker/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': '',
+        'HOST': env("DB_HOST"),
         'PORT': env("DB_PORT"),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
