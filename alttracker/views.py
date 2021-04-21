ddf8cca16b4919219da4a2dfed82b6d49199e190
Joe Farrelly <joefarrelly96@gmail.com>
2021-04-21 21:59:02 +0100

    changed css and js libraries to CDN instead of local and changed home page link

--- alttracker/views.py
+++ alttracker/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
 	# request.session.flush()
-	return render(
-		request,
-		"home/home.html")
+    return redirect('wowprof_home')
+	# return render(
+	# 	request,
+	# 	# "home/home.html"
+ #        "wowprof/wowprof_home.html")
