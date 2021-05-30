00dac211a884499dae9ca83cb798c9d3139a9e13
joefarrelly96@gmail.com <joefarrelly96@gmail.com>
2021-05-30 20:32:39 +0100

    another fix for template filter issue

--- wowprof/templatetags/alttracker_filters.py
+++ wowprof/templatetags/alttracker_filters.py
@@ -27,7 +27,7 @@ def datadiff(value):
     else:
         time = timezone.now() - value
         if time.days >= 1:
-            return (time.days + " day(s) ago")
+            return (str(time.days) + " day(s) ago")
         elif time.seconds >= 3600:
             return (str(int((time.seconds / 60) / 60)) + " hour(s) ago")
         elif time.seconds >= 60:
